19352c2e27d15622fca374e3710085626119f930
Hiep Thai <thaichanhiep@gmail.com>
2017-11-14 16:53:12 +0700

    Fix pattern to match whole string

--- lib/UrlUtils.php
+++ lib/UrlUtils.php
@@ -36,12 +36,24 @@ class UrlUtils {
 		return $inst;
 	}
 
+	/**
+	 * Strip all domain
+	 * @param $url
+	 *
+	 * @return mixed
+	 */
 	public static function stripAllDomain( $url ) {
 
 		return preg_replace( '/^(http)?s?:?\/\/[^\/]*(\/?.*)$/i', '$2', $url );
 
 	}
 
+	/**
+	 * Generate domain pattern
+	 * @param $url
+	 *
+	 * @return string
+	 */
 	private function generateRegex( $url ) {
 		$info = parse_url( $url );
 		$host = $info['host'];
@@ -50,14 +62,22 @@ class UrlUtils {
 		$domainName = array_shift($parts);
 		$domainExt = array_shift($parts);
 
-		return '/^(http)?s?:?\/\/([a-zA-Z\d-]+\.){0,}'. $domainName .'\.'. $domainExt .'/i';
+		return '/(http)?s?:?\/\/([a-zA-Z\d-]+\.){0,}'. $domainName .'\.'. $domainExt .'/i';
 	}
 
-	public function replaceDomain( $url, $newDomain = false ) {
+	/**
+	 * Replace domain with Dcoupled client domain
+	 *
+	 * @param $string
+	 * @param bool $newDomain
+	 *
+	 * @return mixed
+	 */
+	public function replaceDomain( $string, $newDomain = false ) {
 
 		$newDomain = ($newDomain) ? $newDomain: get_option('dcoupled_client_domain', '');
 
-		return preg_replace( $this->domainPattern, $newDomain, $url );
+		return preg_replace( $this->domainPattern, $newDomain, $string );
 	}
 
 }
